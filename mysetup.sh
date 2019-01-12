cbd7b348e63a8573f6dd91e11c44e95b1d19a205
Susumu Seino <seino@susu.mu>
2019-01-12 14:39:40 +0900

    Add cask install list

--- mysetup.sh
+++ mysetup.sh
@@ -1,3 +1,3 @@
 brew install git mysql node ruby php wp-cli wget fish composer yarn
 brew tap caskroom/cask
-brew cask install
+brew cask install sublime-text alfred toggl spotify appcleaner firefox opera libreoffice simple-comic vlc dropbox evernote skype sourcetree phpstorm intellij-ide java docker brackets macdown sequel-pro virtualbox slack google-chrome google-japanese-ime dash poedit
