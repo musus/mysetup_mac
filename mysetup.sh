6f5f46e4d5f77d8aecd994aa51093965e4050503
Susumu Seino <seino@susu.mu>
2019-01-22 12:21:57 +0900

    Add wp-cli

--- mysetup.sh
+++ mysetup.sh
@@ -1,3 +1,8 @@
+cd
 brew install git node ruby php wget fish composer yarn
 brew tap caskroom/cask
 brew cask install sublime-text alfred toggl spotify appcleaner firefox opera libreoffice simple-comic vlc dropbox evernote skype sourcetree intellij-idea java macdown sequel-pro slack google-chrome google-japanese-ime dash poedit adobe-creative-cloud
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
