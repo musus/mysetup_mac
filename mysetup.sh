cb169f4f2ecccfd564ab645a852f66091f4ff3f8
Susumu Seino <seino@susu.mu>
2019-01-22 12:19:36 +0900

    tweak cask list

--- mysetup.sh
+++ mysetup.sh
@@ -1,3 +1,3 @@
 brew install git node ruby php wget fish composer yarn
 brew tap caskroom/cask
-brew cask install sublime-text alfred toggl spotify appcleaner firefox opera libreoffice simple-comic vlc dropbox evernote skype sourcetree intellij-idea java macdown sequel-pro slack google-chrome google-japanese-ime dash poedit 
+brew cask install sublime-text alfred toggl spotify appcleaner firefox opera libreoffice simple-comic vlc dropbox evernote skype sourcetree intellij-idea java macdown sequel-pro slack google-chrome google-japanese-ime dash poedit adobe-creative-cloud
